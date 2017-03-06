41d97e8faccaf399112fde2eb1e798d9c605e1f1
Jacob Nielsen <Jacob Nielsen>
2017-03-05 21:28:48 -0700

    Initial commit

---
+++ app/src/main/java/com/wavefaring/noms_n_bombs/Game.java
@@ -0,0 +1,13 @@
+package com.wavefaring.noms_n_bombs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
